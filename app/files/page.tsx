246289b2d4adc9bd55cf9aab24c6a7295a54b9d7
peterdinis <petter1@pobox.sk>
2025-07-29 13:01:31 +0200

    feat: Remove /files page

--- app/files/page.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import FileManager from "@/components/files/FileManager";
-
-const FilesPage: NextPage = () => {
-	return <FileManager />;
-};
-
-export default FilesPage;
