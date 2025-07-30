fad11f1a62a28bbac224a957767d43bd295fb06f
peterdinis <petter1@pobox.sk>
2025-07-30 18:17:31 +0200

    feat: Updating ability

--- casl/ability.ts
+++ casl/ability.ts
@@ -8,23 +8,31 @@ import {
 
 type Actions = "manage" | "read" | "create" | "update" | "delete";
 
-type Subjects = InferSubjects<"User"> | "all";
+type Subjects = InferSubjects<{
+  User: { id: string };
+  File: { id: string; ownerId: string };
+}> | "all";
 
 export type AppAbility = Ability<[Actions, Subjects]>;
 
-export function defineAbilitiesFor(role: string) {
-  const { can, cannot, build } = new AbilityBuilder<AppAbility>(Ability as AbilityClass<AppAbility>);
+export function defineAbilitiesFor(role: "admin" | "user", userId: string) {
+  const { can, cannot, build } = new AbilityBuilder<AppAbility>(
+    Ability as AbilityClass<AppAbility>
+  );
 
   if (role === "admin") {
     can("manage", "all");
   } else if (role === "user") {
-    can("read", "User");
-    cannot("delete", "User");
-  } else {
-    can("read", "User");
+    can("read", "all");
+    can("create", "all");
+
+    can(["update", "delete"], "all", { ownerId: userId });
+
+    can("read", "all");
+    cannot("delete", "all");
   }
 
   return build({
-    detectSubjectType: (item) => item as ExtractSubjectType<Subjects>,
+    detectSubjectType: (item) => item as  unknown as ExtractSubjectType<Subjects>,
   });
 }
