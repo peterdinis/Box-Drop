6c38079305245b637d8e3feea9c063f792db045d
peterdinis <petter1@pobox.sk>
2025-08-03 12:29:26 +0200

    feat: Working on logic for delete file

--- app/api/files/[id]/route.ts
+++ app/api/files/[id]/route.ts
@@ -1,5 +1,8 @@
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/db";
+import { files } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { UTApi } from "uploadthing/server";
 
 export async function GET(req: Request, context: { params: { id: string } }) {
 	const { userId } = await auth();
@@ -25,4 +28,31 @@ export async function GET(req: Request, context: { params: { id: string } }) {
 		status: 200,
 		headers: { "Content-Type": "application/json" },
 	});
+}
+
+
+export async function DELETE(
+	req: Request,
+	{ params }: { params: { id: string } }
+) {
+	const { userId } = await auth();
+
+	if (!userId) {
+		return new Response("Unauthorized", { status: 401 });
+	}
+	if (!userId) return new Response("Unauthorized", { status: 401 });
+
+	const file = await db.query.files.findFirst({
+		where: eq(files.id, params.id),
+	});
+
+	if (!file) return new Response("Not found", { status: 404 });
+	if (!file.folderId) return new Response("No folder associated", { status: 400 });
+
+	const utapi = new UTApi();
+	await utapi.deleteFiles(file.url);
+
+	await db.delete(files).where(eq(files.id, params.id));
+
+	return new Response("✅ File deleted", { status: 200 });
 }
