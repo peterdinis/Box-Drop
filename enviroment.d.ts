022a72a3d960b4eeaf1fad8edda8cc9559bc6a44
peterdinis <petter1@pobox.sk>
2025-08-06 20:35:46 +0200

    feat: Formatting files

--- enviroment.d.ts
+++ enviroment.d.ts
@@ -1,16 +1,16 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-           NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
-           CLERK_SECRET_KEY: string;
-           NEXT_PUBLIC_CLERK_SIGN_IN_URL: string;
-           NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL: string;
-           NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL: string;
-           UPLOADTHING_TOKEN: string;
-           UPSTASH_REDIS_REST_URL: string;
-           UPSTASH_REDIS_REST_TOKEN: string;
-        }
-    }
+	namespace NodeJS {
+		interface ProcessEnv {
+			NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+			CLERK_SECRET_KEY: string;
+			NEXT_PUBLIC_CLERK_SIGN_IN_URL: string;
+			NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL: string;
+			NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL: string;
+			UPLOADTHING_TOKEN: string;
+			UPSTASH_REDIS_REST_URL: string;
+			UPSTASH_REDIS_REST_TOKEN: string;
+		}
+	}
 }
 
 export {};
