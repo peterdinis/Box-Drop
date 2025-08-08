873dcf5d7b0280945c75e4b2b578dd4ded02e0d4
peterdinis <petter1@pobox.sk>
2025-08-08 14:28:16 +0200

    feat: Formatting files

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,17 +1,17 @@
-import { io, Socket } from 'socket.io-client'
+import { io, type Socket } from "socket.io-client";
 
-let socket: Socket | null = null
+let socket: Socket | null = null;
 
 export const initSocket = (): Socket => {
-  if (!socket) {
-    socket = io({
-      path: '/api/socket',
-    })
+	if (!socket) {
+		socket = io({
+			path: "/api/socket",
+		});
 
-    socket.on('connect', () => {
-      console.log('✅ Connected to socket:', socket?.id)
-    })
-  }
+		socket.on("connect", () => {
+			console.log("✅ Connected to socket:", socket?.id);
+		});
+	}
 
-  return socket
-}
+	return socket;
+};
