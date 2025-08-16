8b6598346f937b0245f5db96b7b4b64f7202552e
peterdinis <petter1@pobox.sk>
2025-08-16 21:38:03 +0200

    feat: New TODO comments

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,8 @@
 import { relations } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+// TODO: Later migration to postgresql
+
 // -------------------- Folders --------------------
 
 export const folders = sqliteTable("folders", {
