6f91cedea9e8be421257f2d806baefebf4585b77
peterdinis <petter1@pobox.sk>
2025-08-18 17:14:30 +0200

    feat: Update DashboardSidebar

--- components/dashboard/DashboardSidebar.tsx
+++ components/dashboard/DashboardSidebar.tsx
@@ -2,7 +2,6 @@
 
 import { HardDrive, Share2, TrendingUp } from "lucide-react";
 import type { FC } from "react";
-import { useAllSharedFiles } from "@/hooks/files/useAllSharedFiles";
 import { useFiles } from "@/hooks/files/useFiles";
 import { useStorageUsage } from "@/hooks/storage/useStorage";
 import { Card } from "../ui/card";
@@ -11,7 +10,6 @@ const DashboardSidebar: FC = () => {
 	const { data: storageUsage } = useStorageUsage();
 	const usedFormatted = storageUsage?.usedFormatted;
 	const { data: filesData } = useFiles();
-	const { data: sharedFilesDta } = useAllSharedFiles();
 
 	return (
 		<div className="space-y-6">
@@ -47,9 +45,6 @@ const DashboardSidebar: FC = () => {
 							<Share2 className="w-4 h-4 text-blue-500" />
 							<span className="text-sm">Files shared</span>
 						</div>
-						<span className="font-medium">
-							{(sharedFilesDta && sharedFilesDta?.length) || 0}
-						</span>
 					</div>
 				</div>
 			</Card>
